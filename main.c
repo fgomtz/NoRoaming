74db876c4145f3f147054e91a0fbee9a7313320f
fgomtz <fgm246@alumnos.unican.es>
2023-04-13 17:57:03 +0200

    Cuarta funcionalidad

--- main.c
+++ main.c
@@ -8,5 +8,6 @@ int main(){
     printf("Funcionalidad botón añadida");
     printf("Añadiendo funcionalidad formulario de envio");
     printf("Funcionalidad formulario de envio añadida");
+    printf("Funcionalidad pasarela de pago añadida");
     return 0;
 }
